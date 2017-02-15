e995acb1c8e9357408810170dee51e0dc49c5a1c
ralf <ralf000@yandex.ru>
2017-02-15 18:51:00 +0300

    +поддержка резервной телепрограммы если основная недоступна или имеет неверные даты

--- app/classes/TVProgram.php
+++ app/classes/TVProgram.php
@@ -40,6 +40,8 @@ class TVProgram extends AFile implements ICreatable
     public function create()
     {
         $InputTVGzData = file_get_contents($this->path);
+        if (!$InputTVGzData || !$this->checkCorrectlyDate())
+            $InputTVGzData = file_get_contents($this->getReserveTvProgramPath());
         $outputTVGzPath = $this->outputTVPath . '.gz';
         file_put_contents($outputTVGzPath, $InputTVGzData);
     }
@@ -50,15 +52,15 @@ class TVProgram extends AFile implements ICreatable
      */
     public function check()
     {
-        $this->gzUnzip();
-        $xml = simplexml_load_file($this->outputTVPath);
-        if (!$xml)
-            throw new FileException('Не удалось открыть ' . $this->outputTVName);
-
+        $xml = $this->getXml($this->outputTVPath);
         $xmlChannels = [];
         foreach ($xml as $item) {
+            /**
+             * @var \SimpleXMLElement $item
+             */
             $xmlChannels[] = MbString::mb_trim((string)$item->{'display-name'});
         }
+
         $playlistChannels = $this->getPlaylistChannels();
         $withoutProgram = [];
         foreach ($playlistChannels as $playlistChannel) {
@@ -74,11 +76,58 @@ class TVProgram extends AFile implements ICreatable
     }
 
     /**
+     * @param string $xmlTvPath
+     * @return \SimpleXMLElement
+     * @throws FileException
+     */
+    private function getXml(string $xmlTvPath) : \SimpleXMLElement
+    {
+        $this->gzUnzip();
+        $xml = simplexml_load_file($xmlTvPath);
+        if (!$xml)
+            throw new FileException('Не удалось открыть ' . $this->outputTVName);
+        return $xml;
+    }
+
+    /**
+     * @return bool
+     * @throws FileException
+     */
+    private function checkCorrectlyDate() : bool
+    {
+        $dates = [];
+        $xml = $this->getXml($this->outputTVPath);
+        foreach ($xml as $item) {
+            /**
+             * @var \SimpleXMLElement $item
+             */
+            if (!$item->{'title'})
+                continue;
+            $date[] = mb_substr($item->attributes()->start, 0, 8);
+        }
+        $minDate = new \DateTime(min($dates));
+        $maxDate = new \DateTime(max($dates));
+        $now = new \DateTime();
+        if (($now < $maxDate) && ($now > $minDate))
+            return true;
+        return false;
+    }
+
+    /**
+     * @return string
+     */
+    private function getReserveTvProgramPath() : string
+    {
+        return $this->path = App::get('config')->get('main.inputReserveTVProgram');
+    }
+
+    /**
      * Отображает каналы без телепрограммы
      * @param array $withoutProgram
      * @return string
      */
-    private function showChannelsWithoutProgram(array $withoutProgram) : string
+    private
+    function showChannelsWithoutProgram(array $withoutProgram) : string
     {
         if (empty($withoutProgram)) {
             $output = '<h3>Для всех телеканалов текущего плейлиста доступна телепрограмма</h3>';
@@ -96,7 +145,8 @@ class TVProgram extends AFile implements ICreatable
     /**
      * @throws FileException
      */
-    private function gzUnzip()
+    private
+    function gzUnzip()
     {
         $tvInput = gzopen($this->path, 'r');
         $tvOutput = fopen($this->outputTVPath, 'w');
@@ -113,7 +163,8 @@ class TVProgram extends AFile implements ICreatable
     /**
      * @return array
      */
-    private function getPlaylistChannels() : array
+    private
+    function getPlaylistChannels() : array
     {
         $playlist = new Playlist();
         $playlist->create();
